a709f21346a70cfc9d931c0d7a2191ea3060cb84
Marc Zaku <mzaku@hotmail.de>
2023-10-11 15:03:09 +0200

    Implements P4Heap

---
+++ P4Heap/Program.cs
@@ -0,0 +1,10 @@
+﻿Player p1 = new Player(); // p1 = #1
+ChangePlayer(p1); // #1
+Console.WriteLine(p1.experience); // #1.experience
+
+
+void ChangePlayer(Player player) // #1
+{
+    player = new Player(); // #2
+    player.experience = 200; // #2.experience = 200
+}
